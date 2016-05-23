50ca2864a4c5d59ba036a9649df618ef6284c5ed
JonBoss5 <jonboss1@hotmail.com>
2016-05-22 23:43:10 -0400

    style profile update

--- PolarBear-website-v2/Controller/updateprofile.php
+++ PolarBear-website-v2/Controller/updateprofile.php
@@ -15,131 +15,133 @@ class updateprofileexception extends exception { }
 
 if (isset ($_POST['uprofile']) && $_POST['uprofile'] == 'Update Profile') {
 
-    $id = $_POST['id'];
-    $email = trim($_POST['email']);
-    $fname = trim($_POST['fname']);
-    $lname = trim($_POST['lname']);
-    $location = trim($_POST['location']);
-    $birthday = $_POST['birthday'];
-    $interests = $_POST['interests'];
-    $image = $_POST['fimg'];
 
-    try {
-        //email validation
-        if (empty($email)) {
-            throw new updateprofileexception ("Please enter your email");
-        } else if (!($valid->checkEmail($email))) {
-            throw new updateprofileexception ("Please enter a valid email");
+        $id = $_POST['id'];
+        $email = trim($_POST['email']);
+        $fname = trim($_POST['fname']);
+        $lname = trim($_POST['lname']);
+        $location = trim($_POST['location']);
+        $birthday = $_POST['birthday'];
+        $interests = $_POST['interests'];
+        $image = $_POST['fimg'];
+
+/*
+        try {
+            //email validation
+            if (empty($email)) {
+                throw new updateprofileexception ("Please enter your email");
+            } else if (!($valid->checkEmail($email))) {
+                throw new updateprofileexception ("Please enter a valid email");
+            }
+            //fname validation
+            if (empty($fname)) {
+                throw new updateprofileexception ("Please enter your first name");
+            }
+            //lname validation
+
+            if (empty($lname)) {
+                throw new updateprofileexception ("Please enter your last name");
+            }
+
+            //location catch
+
+            if (empty($location)) {
+                throw new updateprofileexception ("Please enter your postal code");
+            } else if (!($valid->checkPostalCode($location))) {
+                throw new updateprofileexception ("Please enter a valid postal code");
+            }
+
+            //birthday validation
+
+            if (empty($birthday)) {
+                throw new updateprofileexception ("Please enter your birthdate");
+            } else if (!($valid->checkDate($birthday))) {
+                throw new updateprofileexception ("Please enter the date in the following format yyyy-mm-dd");
+            }
+
+
+            //interests validate
+
+            if (empty($interests)) {
+                throw new updateprofileexception  ("Please enter a few interests");
+            }
+        }catch
+        (exception $e){
+            echo 'Caught exception: ', $e->getMessage(), "\n";
         }
-        //fname validation
-        if (empty($fname)) {
-            throw new updateprofileexception ("Please enter your first name");
-        }
-        //lname validation
 
-        if (empty($lname)) {
-            throw new updateprofileexception ("Please enter your last name");
-        }
-
-        //location catch
-
-        if (empty($location)) {
-            throw new updateprofileexception ("Please enter your postal code");
-        } else if (!($valid->checkPostalCode($location))) {
-            throw new updateprofileexception ("Please enter a valid postal code");
-        }
-
-        //birthday validation
-
-        if (empty($birthday)) {
-            throw new updateprofileexception ("Please enter your birthdate");
-        } else if (!($valid->checkDate($birthday))) {
-            throw new updateprofileexception ("Please enter the date in the following format yyyy-mm-dd");
-        }
-
-
-        //interests validate
-
-        if (empty($interests)) {
-            throw new updateprofileexception  ("Please enter a few interests");
-        }
-    }catch
-    (exception $e){
-        echo 'Caught exception: ', $e->getMessage(), "\n";
     }
 
-}
 
-
-/*THIS PART WAS ADDED FROM AN IN CLASS EXAMPLE*/
+    /*THIS PART WAS ADDED FROM AN IN CLASS EXAMPLE*/
 //get the variable values in superglobles $_FILES array
 //need to save the images to a folder in the project and I can save the path to the image
 //in the database
 //path of the file in temp directory
-$file_temp = $_FILES['fimg']['tmp_name'];
-//original path and file name of the uploaded file
-$file_name = $_FILES['fimg']['name'];
-//size of the uploaded file in bytes
-$file_size = $_FILES['fimg']['size'];
-//type of the file(if browser provides)
-$file_type = $_FILES['fimg']['type'];
-//error number
+    /*$file_temp = $_FILES['fimg']['tmp_name'];
+    //original path and file name of the uploaded file
+    $file_name = $_FILES['fimg']['name'];
+    //size of the uploaded file in bytes
+    $file_size = $_FILES['fimg']['size'];
+    //type of the file(if browser provides)
+    $file_type = $_FILES['fimg']['type'];
+    //error number
 
-$file_error = $_FILES['fimg']['error'];
+    $file_error = $_FILES['fimg']['error'];
 
 
-if ($file_error > 0)
-{
-    echo "Problem";
-    switch ($file_error)
+    if ($file_error > 0)
     {
-        case 1:
-            echo "File exceeded upload_max_filesize.";
-            break;
-        case 2:
-            echo "File exceeded max_file_size";
-            break;
-        case 3:
-            echo "File only partially uploaded.";
-            break;
-        case 4:
-            echo "No file uploaded.";
-            break;
+        echo "Problem";
+        switch ($file_error)
+        {
+            case 1:
+                echo "File exceeded upload_max_filesize.";
+                break;
+            case 2:
+                echo "File exceeded max_file_size";
+                break;
+            case 3:
+                echo "File only partially uploaded.";
+                break;
+            case 4:
+                echo "No file uploaded.";
+                break;
+        }
+        exit;
     }
-    exit;
-}
-class addimageexception extends exception { }
-$max_file_size = 300000;
-try {
-    if ($file_size > $max_file_size) {
-        throw new addimageexception ("file size too big");
+    class addimageexception extends exception { }
+    $max_file_size = 300000;
+    try {
+        if ($file_size > $max_file_size) {
+            throw new addimageexception ("file size too big");
 
+        }
+    } catch (exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
     }
-} catch (exception $e) {
-    echo 'Caught exception: ', $e->getMessage(), "\n";
-}
-//This is the folder where the images will be saved
-$target_path = "../images/uploads/";
-$target_path = $target_path .  $_FILES['fimg']['name'];
-$image = $_FILES['fimg']['name'];
-//move the uploaded file from tempe path to taget path
-try {
-    if(!move_uploaded_file($_FILES['fimg']['tmp_name'], $target_path)) {
-        throw new addimageexception ("There was an error uploading the file, please try again!");
+    //This is the folder where the images will be saved
+    $target_path = "../images/uploads/";
+    $target_path = $target_path .  $_FILES['fimg']['name'];
+    $image = $_FILES['fimg']['name'];
+    //move the uploaded file from tempe path to taget path
+    try {
+        if(!move_uploaded_file($_FILES['fimg']['tmp_name'], $target_path)) {
+            throw new addimageexception ("There was an error uploading the file, please try again!");
+        }
     }
-}
-catch (exception $e) {
-    echo  $e->getMessage(), "\n";
-}
+    catch (exception $e) {
+        echo  $e->getMessage(), "\n";
+    }*/
 
-    $sql="UPDATE user_profiles
+    $sql = "UPDATE user_profiles
             SET email = :email, fname = :fname, lname = :lname, image = :image, location = :location, birthday = :birthday, interests = :interests, full_name = CONCAT(:fname,' ',:lname)
             WHERE id = :id"; //need to put the variables in single quotes since these are strings if you are inserting variables
     //:name ect is a place holder that we use when we are going to user param statments to prevent sql injection
-     //$row = $db->exec($sql); //use when you want to insert update and delete
+    //$row = $db->exec($sql); //use when you want to insert update and delete
     //use query statement only when you want to list things from the database
-   $stm=$db->prepare($sql); //this is called a prepared statement, we are using it instad of the exec statment
-   //the prepare statment is to make sure that no code commands are passed through to the webiste
+    $stm = $db->prepare($sql); //this is called a prepared statement, we are using it instad of the exec statment
+    //the prepare statment is to make sure that no code commands are passed through to the webiste
     $stm->bindValue(':email', $email, PDO::PARAM_STR);
     $stm->bindValue(':fname', $fname, PDO::PARAM_STR);
     $stm->bindValue(':lname', $lname, PDO::PARAM_STR);
@@ -147,13 +149,13 @@ catch (exception $e) {
     $stm->bindValue(':location', $location, PDO::PARAM_STR);
     $stm->bindValue(':birthday', $birthday, PDO::PARAM_STR);
     $stm->bindValue(':interests', $interests, PDO::PARAM_STR);
-    $stm->bindValue(':id',$id);
+    $stm->bindValue(':id', $id);
 
 
-  //$row=$stm->execute(array(':fname' => $fname, ':lname' => $lname,':password'=> $password, ':image'=>$image, ':location'=> $location, ':birthday'=>$birthday, ':interests'=>$interests ));
+    //$row=$stm->execute(array(':fname' => $fname, ':lname' => $lname,':password'=> $password, ':image'=>$image, ':location'=> $location, ':birthday'=>$birthday, ':interests'=>$interests ));
     $row = $stm->execute();
     header("location:../View/showprofile.php");
     //var_dump($row);
 
-
+}
 ?>
--- PolarBear-website-v2/View/updateprofileform.php
+++ PolarBear-website-v2/View/updateprofileform.php
@@ -16,19 +16,50 @@ $user_profiles = $result->fetch();
 require_once "header.php";
 ?>
 <!--This is the form for entering the new values for the profile-->
-<form action="../Controller/updateprofile.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="id" value="<?php echo $user_profiles ['id'];?>"/>
-    Email <input type="text" name="email" value="<?php echo $user_profiles['email'];?>"> <br />
-    First name<input type="text" name="fname" value="<?php echo $user_profiles['fname'];?>"> <br />
-    Last name<input type="text" name="lname" value="<?php echo $user_profiles['lname'];?>"> <br />
-    Postal code <input type="text" name="location" value="<?php echo $user_profiles['location'];?>"><br />
-    Birthday <input type="date" name="birthday" value="<?php echo $user_profiles['birthday'];?>"><br />
-    Interests <input type="text" name="interests" value="<?php echo $user_profiles['interests'];?>"><br />
+<div class="container">
+    <form action="../Controller/updateprofile.php" method="post" enctype="multipart/form-data" class="col-md-8 col-md-offset-2">
+        <h1>Update Profile</h1>
 
-    <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
-    Select an image to upload: <input type="file" name="fimg" id="fimg" ><br />
-    <input type="submit" value="Update Profile" name="uprofile"> <!--it always needs to have a name -->
-</form>
+        <input type="hidden" name="id" value="<?php echo $user_profiles ['id'];?>"/>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email" class="form-control input-lg" <?php echo 'value=' . $user_profiles['email'];?>>
+            </div>
+            <div class="form-group">
+                <label for="fname">First Name:</label>
+                <input type="text" name="fname" id="fname" class="form-control input-lg" <?php echo 'value=' . $user_profiles['fname'];?>>
+            </div>
+            <div class="form-group">
+                <label for="lname">Last Name:</label>
+                <input type="text" name="lname" id="lname" class="form-control input-lg" <?php echo 'value=' . $user_profiles['lname'];?>>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="location">Postal Code:</label>
+                <input type="text" name="location" id="location" class="form-control input-lg" <?php echo 'value=' . $user_profiles['location'];?>>
+            </div>
+            <div class="form-group">
+                <label for="birthday">Birthday:</label>
+                <input type="text" name="birthday" id="birthday" class="form-control input-lg" <?php echo 'value=' .  $user_profiles['birthday'];?>>
+            </div>
+            <div class="form-group">
+                <label for="interests">Interests:</label>
+                <input type="text" name="interests" id="interests" class="form-control input-lg" <?php echo 'value=' . $user_profiles['interests'];?>>
+            </div>
+            <div class="form-group">
+                <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+                <label for="interests">Profile Image:</label>
+                <input type="file" name="fimg" id="fimg" class="form-control input-lg"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Update Profile" id="aprofile" name="uprofile" class="btn btn-info btn-block"> <!--it always needs to have a name -->
+        </div>
+    </form>
+</div>
+
 <?php
 require_once "footer.php";
 ?>
