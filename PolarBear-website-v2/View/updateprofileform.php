a78c6259842b1ac2587b789b69043968724884c1
yipiki@gmail.com <yipiki@gmail.com>
2016-05-23 02:56:39 -0400

    touch up updateprofileform style

--- PolarBear-website-v2/View/updateprofileform.php
+++ PolarBear-website-v2/View/updateprofileform.php
@@ -17,47 +17,52 @@ require_once "header.php";
 ?>
 <!--This is the form for entering the new values for the profile-->
 <div class="container">
-    <form action="../Controller/updateprofile.php" method="post" enctype="multipart/form-data" class="col-md-8 col-md-offset-2">
-        <h1>Update Profile</h1>
+    <div class="row">
+        <form action="../Controller/updateprofile.php" method="post" enctype="multipart/form-data" class="col-md-6 col-md-offset-3">
+            <div class="row">
+                <h1>Update Profile</h1>
 
-        <input type="hidden" name="id" value="<?php echo $user_profiles ['id'];?>"/>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="text" name="email" id="email" class="form-control input-lg" <?php echo 'value=' . $user_profiles['email'];?>>
+                <input type="hidden" name="id" value="<?php echo $user_profiles ['id'];?>"/>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" id="email" class="form-control" <?php echo 'value=' . $user_profiles['email'];?>>
+                    </div>
+                    <div class="form-group">
+                        <label for="fname">First Name:</label>
+                        <input type="text" name="fname" id="fname" class="form-control" <?php echo 'value=' . $user_profiles['fname'];?>>
+                    </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name:</label>
+                        <input type="text" name="lname" id="lname" class="form-control" <?php echo 'value=' . $user_profiles['lname'];?>>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label for="location">Postal Code:</label>
+                        <input type="text" name="location" id="location" class="form-control" <?php echo 'value=' . $user_profiles['location'];?>>
+                    </div>
+                    <div class="form-group">
+                        <label for="birthday">Birthday:</label>
+                        <input type="text" name="birthday" id="birthday" class="form-control" <?php echo 'value=' .  $user_profiles['birthday'];?>>
+                    </div>
+                    <div class="form-group">
+                        <label for="interests">Interests:</label>
+                        <input type="text" name="interests" id="interests" class="form-control" <?php echo 'value=' . $user_profiles['interests'];?>>
+                    </div>
+                </div>
+                <div class="form-group col-sm-12">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+                    <label for="interests">Profile Image:</label>
+                    <input type="file" name="fimg" id="fimg" class="form-control"/>
+                </div>
+                <div class="form-group col-sm-12">
+                    <input type="submit" value="Update Profile" id="aprofile" name="uprofile" class="btn btn-info btn-block"> <!--it always needs to have a name -->
+                    <a href="showprofile.php" class="btn btn-default btn-block">Cancel</a>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="fname">First Name:</label>
-                <input type="text" name="fname" id="fname" class="form-control input-lg" <?php echo 'value=' . $user_profiles['fname'];?>>
-            </div>
-            <div class="form-group">
-                <label for="lname">Last Name:</label>
-                <input type="text" name="lname" id="lname" class="form-control input-lg" <?php echo 'value=' . $user_profiles['lname'];?>>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="location">Postal Code:</label>
-                <input type="text" name="location" id="location" class="form-control input-lg" <?php echo 'value=' . $user_profiles['location'];?>>
-            </div>
-            <div class="form-group">
-                <label for="birthday">Birthday:</label>
-                <input type="text" name="birthday" id="birthday" class="form-control input-lg" <?php echo 'value=' .  $user_profiles['birthday'];?>>
-            </div>
-            <div class="form-group">
-                <label for="interests">Interests:</label>
-                <input type="text" name="interests" id="interests" class="form-control input-lg" <?php echo 'value=' . $user_profiles['interests'];?>>
-            </div>
-            <div class="form-group">
-                <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
-                <label for="interests">Profile Image:</label>
-                <input type="file" name="fimg" id="fimg" class="form-control input-lg"/>
-            </div>
-        </div>
-        <div class="form-group">
-            <input type="submit" value="Update Profile" id="aprofile" name="uprofile" class="btn btn-info btn-block"> <!--it always needs to have a name -->
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
 
 <?php
